ea0b2c0422fb4a89d809276fb32c9e0444146aec
1oneegame <95096078+1oneegame@users.noreply.github.com>
2024-09-07 21:56:37 +0500

    Added my project

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,44 @@
 'use client'
 import { useState } from 'react';
 import ProfileCard from '@/components/profile/profilecard';
+import { TrendingUp } from "lucide-react"
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+
+const chartData = [
+  { month: "January", desktop: 5 },
+  { month: "February", desktop: 6 },
+  { month: "March", desktop: 7 },
+  { month: "April", desktop: 8 },
+  { month: "May", desktop: 9 },
+  { month: "June", desktop: 10 },
+  { month: "July", desktop: 11 },
+  { month: "August", desktop: 12 },
+  { month: "September", desktop: 13 },
+  { month: "October", desktop: 14 },
+  { month: "November", desktop: 15 },
+  { month: "December", desktop: 16 },
+]
+
+const chartConfig = {
+  desktop: {
+    label: "Прочитанные книги",
+    color: "hsl(var(--chart-1))",
+  }
+} satisfies ChartConfig
 
 export default function ProfilePage() {
   const [profile, setProfile] = useState({
@@ -22,8 +60,44 @@ export default function ProfilePage() {
         />
       </div>
       <div className="flex-grow p-6 ml-6 bg-white rounded-lg shadow-md">
-        <h3 className="text-xl font-semibold mb-4">Materials</h3>
-        <p>...</p>
+        <Card>
+          <CardHeader>
+            <CardTitle>Ваша статистика: </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="flex space-x-6">
+              <div className="w-96 h-72">
+                <ChartContainer config={chartConfig}>
+                  <BarChart accessibilityLayer data={chartData}>
+                    <CartesianGrid vertical={false} />
+                    <XAxis
+                      dataKey="month"
+                      tickLine={false}
+                      tickMargin={10}
+                      axisLine={false}
+                      tickFormatter={(value) => value.slice(0, 3)}
+                    />
+                    <ChartTooltip
+                      cursor={false}
+                      content={<ChartTooltipContent indicator="dashed" />}
+                    />
+                    <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+                  </BarChart>
+                </ChartContainer>
+              </div>
+
+              {/* Блок с достижениями */}
+              <div className="flex flex-col items-center justify-center p-4 bg-gray-200 border-2 border-gray-300 rounded-lg shadow-lg ml-24">
+                <img
+                  src="https://example.com/achievement-image.png"
+                  alt="Достижение"
+                  className="w-24 h-24 mb-4 rounded-full"
+                />
+                <p className="text-lg font-semibold">Ваши достижения</p>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
